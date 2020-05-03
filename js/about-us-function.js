8a4e79d72ab3d6e5ba19b0eacb59210763fdb1ee
Himanshu Dagdi <dagdih@gmail.com>
2020-05-03 08:02:45 +0530

    added some pages

---
+++ js/about-us-function.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function($) {
+    "use strict";
+    $('#testimonials-list').owlCarousel({
+        loop: true,
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots:true,
+        autoplayTimeout: 4500,
+        smartSpeed: 450,
+        responsive: {
+          0: {
+            items: 1
+          },
+          768: {
+            items: 2
+          },
+          1170: {
+            items: 3
+          }
+        }
+    });
+});
---
+++ js/contact-function.js
@@ -0,0 +1,42 @@
+// Code By Webdevtrick ( https://webdevtrick.com )
+$('textarea').blur(function () {
+    $('#contactME textarea').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('textarea + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('textarea + label + span').css({'opacity': 0});
+        }
+    });
+});
+
+$('#contactME .field:first-child input').blur(function () {
+    $('#contactME .field:first-child input').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('.field:first-child input + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('.field:first-child input + label + span').css({'opacity': 0});
+        }
+    });
+});
+
+$('#contactME .field:nth-child(2) input').blur(function () {
+    $('#contactME .field:nth-child(2) input').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('.field:nth-child(2) input + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('.field:nth-child(2) input + label + span').css({'opacity': 0});
+        }
+    });
+});
